04465492e4de0fa98bd2cb2cf9612be14bfa1aef
BaltaKlixJones <balta98_@hotmail.com>
2023-06-09 16:50:12 -0300

    authRoutes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,5 +33,50 @@ export const register = async (req, res) => {
 
 
 export const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  try {
+
+    const userFound = await User.findOne({ email });
+
+    if (!userFound) return res.status(400).json({ message: "User not found" });
+
+    const isMatch = await bcrypt.compare(password, userFound.password);
+
+    if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
+
+
+    const token = await createAccesToken({ id: userFound._id });
+
+    res.cookie("token", token);
+
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
+
+export const logout = async (req, res) => {
+  res.cookie("token", "", { expires : new Date(0) });
+  res.json({ message: "Logout successfully" });
+}
+
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id)
+  if (!userFound) return res.status(400).json({ message: "User not found" })
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  })
+  
+}
